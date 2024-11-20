42554cfa2511f9966cb276b0aafffaf8f3f05ad5
njemecc <branislannjemec@gmail.com>
2024-11-20 14:00:48 +0100

    create DbContext

---
+++ matchMaking.be/Context/MatchmakingContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+using matchMaking.be.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace matchMaking.be.Context;
+
+public class MatchmakingContext: DbContext
+{
+
+    public MatchmakingContext(DbContextOptions<MatchmakingContext> options):base(options)
+    {
+        
+    }
+    
+    public DbSet<Player> Players { get; set; }
+    public DbSet<Team> Teams { get; set; }
+    
+}
