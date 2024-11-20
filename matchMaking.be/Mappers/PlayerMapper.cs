f92893f856829ce326e8e16c56e15d7182982928
njemecc <branislannjemec@gmail.com>
2024-11-20 14:33:21 +0100

    create Function that is mapping from model to response dto of getting user by id

--- matchMaking.be/Mappers/PlayerMapper.cs
+++ matchMaking.be/Mappers/PlayerMapper.cs
@@ -24,6 +24,7 @@ public static class PlayerMapper
     {
         return new CreatePlayerResponseDto()
         {
+            Id = Guid.NewGuid(),
             Nickname = playerRequestDto.Nickname,
             Wins = 0,
             Losses = 0,
@@ -36,11 +37,28 @@ public static class PlayerMapper
     {
         return new CreatePlayerResponseDto()
         {
+            Id = player.Id,
             Nickname = player.Nickname,
             Wins = player.Wins,
             Losses = player.Losses,
             Elo = player.Elo,
             HoursPlayed = player.HoursPlayed
+        };
+    }
+
+    public static GetPlayerByIdResponse FromModelToGetPlayerByIdResponse(this Player player)
+    {
+        return new GetPlayerByIdResponse()
+        {
+            Id = player.Id,
+            Nickname = player.Nickname,
+            Wins = player.Wins,
+            Losses = player.Losses,
+            Elo = player.Elo,
+            HoursPlayed = player.HoursPlayed,
+            RatingAdjustment = player.RatingAdjustment,
+            TeamId = player.TeamId
+
         };
     }
 }
