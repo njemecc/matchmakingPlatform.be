3b038fd0362294e02edf9d2d1ad86d9d6279c048
njemecc <branislannjemec@gmail.com>
2024-11-20 22:12:45 +0100

    create Matches Controller

---
+++ matchMaking.be/Controllers/MatchController.cs
@@ -0,0 +1,35 @@
+﻿using matchMaking.be.Dtos.Match;
+using matchMaking.be.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace matchMaking.be.Controllers;
+
+[ApiController]
+[Route("matches")]
+public class MatchesController : ControllerBase
+{
+    private readonly IMatchService _matchService;
+
+    public MatchesController(IMatchService matchService)
+    {
+        _matchService = matchService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddMatch([FromBody] CreateMatchRequestDto createMatchRequestDto)
+    {
+        try
+        {
+            var match = await _matchService.AddMatch(createMatchRequestDto);
+            return Ok(match);
+        }
+        catch (KeyNotFoundException ex)
+        {
+            return NotFound(new { error = ex.Message });
+        }
+        catch (InvalidOperationException ex)
+        {
+            return BadRequest(new { error = ex.Message });
+        }
+    }
+}
