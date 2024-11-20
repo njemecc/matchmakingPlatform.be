581a121c84bfb0a7841eda73c1b395bea2442cfe
njemecc <branislannjemec@gmail.com>
2024-11-20 13:59:23 +0100

    create Team model

---
+++ matchMaking.be/Models/Team.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace matchMaking.be.Models;
+
+public class Team
+{
+    [Key]
+    
+    public Guid Id { get; set; }
+    
+    public string TeamName { get; set; }
+    
+    public ICollection<Player> Players { get; set; }
+    
+}
