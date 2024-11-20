e438477bc1f236989d061c996354dfab11a350f0
njemecc <branislannjemec@gmail.com>
2024-11-20 15:44:08 +0100

    create custom exceptions

---
+++ matchMaking.be/Exceptions/PlayerNotFoundException.cs
@@ -0,0 +1,7 @@
+﻿namespace matchMaking.be.Exceptions;
+
+
+    public class PlayerNotFoundException : Exception
+    {
+        public PlayerNotFoundException(string message) : base(message) { }
+    }
