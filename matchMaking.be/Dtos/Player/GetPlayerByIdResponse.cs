3a43463ef32ff7687e1254129ca8fc151eb2724e
njemecc <branislannjemec@gmail.com>
2024-11-20 14:32:51 +0100

    create GetPlayerByIdResponse Dto

---
+++ matchMaking.be/Dtos/Player/GetPlayerByIdResponse.cs
@@ -0,0 +1,20 @@
+﻿namespace matchMaking.be.Dto.Player;
+
+public class GetPlayerByIdResponse
+{
+    public Guid Id { get; set; }
+    
+    public string Nickname { get; set; } 
+    
+    public int Wins { get; set; }
+    
+    public int Losses { get; set; }
+    
+    public int Elo { get; set; }
+    
+    public int HoursPlayed { get; set; }
+    
+    public int RatingAdjustment { get; set; }
+    
+    public Guid? TeamId { get; set; }
+}
