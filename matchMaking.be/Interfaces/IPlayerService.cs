a771c5f2b47ea05de5e4d47b0d2554258b40c24a
njemecc <branislannjemec@gmail.com>
2024-11-20 15:43:56 +0100

    create playerService

---
+++ matchMaking.be/Interfaces/IPlayerService.cs
@@ -0,0 +1,10 @@
+﻿using matchMaking.be.Dto.Player;
+using matchMaking.be.Models;
+
+namespace matchMaking.be.Interfaces;
+
+public interface IPlayerService
+{
+    Task<CreatePlayerResponseDto?> CreatePlayer(CreatePlayerRequestDto createPlayerRequestDto);
+    Task<GetPlayerByIdResponse?> GetPlayerById(Guid playerId);
+}
